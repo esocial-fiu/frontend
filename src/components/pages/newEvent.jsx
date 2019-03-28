5083fb2a522daf9b33dfd1646a7e705f34040968
Vicente Maselli <vmase001@fiu.edu>
2019-03-28 17:59:42 -0400

    new event

--- src/components/pages/newEvent.jsx
+++ src/components/pages/newEvent.jsx
@@ -1,9 +1,126 @@
 import React, { Component } from "react";
+import { Col, Form, Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Axios from "axios";
 
 class NewEvent extends Component {
-    render() { 
-        return (  <h1>hhhh</h1>);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: null,
+      newEvent: {
+        new_title: "",
+        new_description: "",
+        new_location: "",
+        new_date: "",
+        new_max: "",
+        new_attendees: ["createdBy"]
+      }
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    var newEvent = this.state.newEvent;
+    newEvent[event.target.name] = event.target.value;
+    this.setState({
+      newEvent
+    });
+  }
+
+  render() {
+    return (
+      <div
+        className="container"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <Card
+          style={{
+            width: "70%",
+            marginTop: "30px"
+          }}
+        >
+          <Card.Body>
+            <Form>
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridFname">
+                  <Form.Label>Title</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter a title"
+                    name="new_title"
+                    value={this.state.newEvent.new_title}
+                    onChange={this.onChange}
+                  />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridLname">
+                  <Form.Label>Description</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter a description"
+                    name="new_description"
+                    value={this.state.newEvent.new_description}
+                    onChange={this.onChange}
+                  />
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridEmail">
+                  <Form.Label>Location</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter location"
+                    name="new_location"
+                    value={this.state.newEvent.new_location}
+                    onChange={this.onChange}
+                  />
+                </Form.Group>
+              </Form.Row>
+              <Form.Group controlId="formGridBirthday">
+                <Form.Label>Date</Form.Label>
+                <Form.Control
+                  placeholder="MM/DD/YYYY"
+                  name="new_date"
+                  value={this.state.newEvent.new_date}
+                  onChange={this.onChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="formGridPassword">
+                <Form.Label>Max capacity</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="new_max"
+                  value={this.state.newEvent.new_max}
+                  onChange={this.onChange}
+                />
+              </Form.Group>
+              <Form.Group id="formGridCheckbox">
+                <Form.Check
+                  type="checkbox"
+                  required
+                  label="Agree to terms and conditions"
+                />
+              </Form.Group>
+              <Button variant="dark" type="submit" onClick={this.submit}>
+                <Link to="/profile" style={{ color: "white" }}>
+                  Submit
+                </Link>
+              </Button>
+              &nbsp;
+              <Button variant="dark" type="submit">
+                {" "}
+                <Link to="/" style={{ color: "white" }}>
+                  Cancel
+                </Link>
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  }
 }
- 
+
 export default NewEvent;
